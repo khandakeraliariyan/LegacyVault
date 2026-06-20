8c0611f366a087bf2cfc90d162af8bdf903cdaad
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-21 02:38:51 +0600

    added the initial app and server

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "LegacyVault API Running",
+    });
+});
+
+module.exports = app;

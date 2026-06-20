a6c24a8ee5fc3b11eedffad2b81d01781448f099
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-21 02:44:38 +0600

    added the user routes

---
+++ server/src/modules/user/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        firebaseUid: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        role: {
+            type: String,
+            enum: ["USER", "SUCCESSOR", "ADMIN"],
+            default: "USER",
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model(
+    "User",
+    userSchema
+);

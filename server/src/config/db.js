62930b5321e7cf9bd2e536bf29cd4849a5b959b2
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-21 02:37:16 +0600

    added the db config file

---
+++ server/src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log(error);
+
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;

4b4565d3ac164f690b7225a3f7edc3fa88849ab4
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-21 11:34:26 +0600

    created the successor controller function

---
+++ server/src/modules/successor/successor.controller.js
@@ -0,0 +1,76 @@
+const successorService = require("./successor.service");
+
+const createSuccessor = async (req, res, next) => {
+    try {
+        const successor =
+            await successorService.createSuccessor(
+                req.user._id,
+                req.body
+            );
+
+        res.status(201).json({
+            success: true,
+            message:
+                "Successor added successfully",
+            data: successor,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getMySuccessor = async (req, res, next) => {
+    try {
+        const successor =
+            await successorService.getMySuccessor(
+                req.user._id
+            );
+
+        res.status(200).json({
+            success: true,
+            data: successor,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateSuccessor = async (req, res, next) => {
+    try {
+        const successor =
+            await successorService.updateSuccessor(
+                req.user._id,
+                req.body
+            );
+
+        res.status(200).json({
+            success: true,
+            data: successor,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteSuccessor = async (req, res, next) => {
+    try {
+        await successorService.deleteSuccessor(
+            req.user._id
+        );
+
+        res.status(200).json({
+            success: true,
+            message:
+                "Successor deleted successfully",
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    createSuccessor,
+    getMySuccessor,
+    updateSuccessor,
+    deleteSuccessor,
+};

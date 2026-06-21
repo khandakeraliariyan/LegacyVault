c213af55f2e3af47de5e068c440b59c94092d17a
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-21 11:25:02 +0600

    added the router and connected with app

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 
+const routes = require("./routes");
+
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/api/v1", routes);
 
 app.use(cookieParser());
 
